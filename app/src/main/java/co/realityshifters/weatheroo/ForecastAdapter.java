9e17bccc9a02527d684eaa6f7750e3782af27244
Yeap Hooi Tong <hooitongx@gmail.com>
2017-03-08 11:27:30 +0800

    Implemented RecyclerView for list of weather data

---
+++ app/src/main/java/co/realityshifters/weatheroo/ForecastAdapter.java
@@ -0,0 +1,68 @@
+package co.realityshifters.weatheroo;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastViewHolder> {
+
+    final private ForecastClickListener mClickListener;
+
+    private String[] mWeatherData;
+
+    public interface ForecastClickListener {
+        void onForecastClick(String weather);
+    }
+
+    public ForecastAdapter(String[] weatherData, ForecastClickListener listener) {
+        mWeatherData = weatherData;
+        mClickListener = listener;
+    }
+
+
+    @Override
+    public ForecastViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.forecast_list_item, parent, false);
+        ForecastViewHolder viewHolder = new ForecastViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ForecastViewHolder holder, int position) {
+        holder.bind(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mWeatherData == null ? 0 : mWeatherData.length;
+    }
+
+    public void setWeatherData(String[] weatherData) {
+        mWeatherData = weatherData;
+        notifyDataSetChanged();
+    }
+
+    class ForecastViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public final TextView mWeatherTextView;
+
+        public ForecastViewHolder(View itemView) {
+            super(itemView);
+            mWeatherTextView = (TextView) itemView.findViewById(R.id.tv_weather_data);
+            itemView.setOnClickListener(this);
+        }
+
+        void bind(int position) {
+            mWeatherTextView.setText(mWeatherData[position]);
+        }
+
+        @Override
+        public void onClick(View v) {
+            mClickListener.onForecastClick(mWeatherTextView.getText().toString());
+        }
+    }
+}
--- app/src/main/java/co/realityshifters/weatheroo/MainActivity.java
+++ app/src/main/java/co/realityshifters/weatheroo/MainActivity.java
@@ -1,39 +1,127 @@
 package co.realityshifters.weatheroo;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.net.URL;
 
-    TextView mWeatherList;
+import co.realityshifters.weatheroo.data.SunshinePreferences;
+import co.realityshifters.weatheroo.utilities.NetworkUtils;
+import co.realityshifters.weatheroo.utilities.OpenWeatherJsonUtils;
+
+public class MainActivity extends AppCompatActivity implements
+        ForecastAdapter.ForecastClickListener {
+
+    private TextView mErrorMessage;
+    private ProgressBar mProgressBar;
+
+    private RecyclerView mRecyclerView;
+    private ForecastAdapter mForecastAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
 
-        mWeatherList = (TextView) findViewById(R.id.tv_weather_data);
+        mRecyclerView = (RecyclerView) findViewById(R.id.rv_forecast);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this,
+                LinearLayoutManager.VERTICAL, false);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setHasFixedSize(true);
 
-        String[] dummyWeatherData = {
-                "Today, May 17 - Clear - 17°C / 15°C",
-                "Tomorrow - Cloudy - 19°C / 15°C",
-                "Thursday - Rainy- 30°C / 11°C",
-                "Friday - Thunderstorms - 21°C / 9°C",
-                "Saturday - Thunderstorms - 16°C / 7°C",
-                "Sunday - Rainy - 16°C / 8°C",
-                "Monday - Partly Cloudy - 15°C / 10°C",
-                "Tue, May 24 - Meatballs - 16°C / 18°C",
-                "Wed, May 25 - Cloudy - 19°C / 15°C",
-                "Thu, May 26 - Stormy - 30°C / 11°C",
-                "Fri, May 27 - Hurricane - 21°C / 9°C",
-                "Sat, May 28 - Meteors - 16°C / 7°C",
-                "Sun, May 29 - Apocalypse - 16°C / 8°C",
-                "Mon, May 30 - Post Apocalypse - 15°C / 10°C",
-        };
+        mForecastAdapter = new ForecastAdapter(null, this);
+        mRecyclerView.setAdapter(mForecastAdapter);
 
-        for (String mockData : dummyWeatherData) {
-            mWeatherList.append(mockData + "\n\n\n");
+        mErrorMessage = (TextView) findViewById(R.id.tv_error_message);
+
+        mProgressBar = (ProgressBar) findViewById(R.id.pg_loading_progress);
+        loadWeatherData();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.forecast, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_refresh:
+                mForecastAdapter.setWeatherData(null);
+                loadWeatherData();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void loadWeatherData() {
+        String location = SunshinePreferences.getPreferredWeatherLocation(this);
+        new FetchWeatherTask().execute(location);
+    }
+
+    @Override
+    public void onForecastClick(String weather) {
+        Toast.makeText(this, weather, Toast.LENGTH_SHORT).show();
+    }
+
+    private void showWeatherDataView() {
+        mErrorMessage.setVisibility(View.INVISIBLE);
+        mRecyclerView.setVisibility(View.VISIBLE);
+    }
+
+    private void showErrorMessage() {
+        mRecyclerView.setVisibility(View.INVISIBLE);
+        mErrorMessage.setVisibility(View.VISIBLE);
+    }
+
+    public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String[] doInBackground(String... params) {
+            if (params.length == 0) return null;
+
+            String location = params[0];
+            URL weatherRequest = NetworkUtils.buildUrl(location);
+
+            try {
+                String response = NetworkUtils.getResponseFromHttpUrl(weatherRequest);
+                String[] jsonData = OpenWeatherJsonUtils.getSimpleWeatherStringsFromJson(
+                        MainActivity.this, response);
+                return jsonData;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String[] weatherData) {
+            super.onPostExecute(weatherData);
+            mProgressBar.setVisibility(View.INVISIBLE);
+            if (weatherData != null) {
+                showWeatherDataView();
+                mForecastAdapter.setWeatherData(weatherData);
+            } else {
+                showErrorMessage();
+            }
         }
     }
 }
